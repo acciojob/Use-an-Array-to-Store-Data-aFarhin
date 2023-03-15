b98937cc13fc1472a6617b797f79f4edbc0d8894
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-15 11:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 
 let yourArray = [1, " two", true, 5, "one"];
-document.getElementById("demo").innerHTML = yourArray;
+
+let Demo= document.getElementById('demo');
+Demo.innerHTML ='';
+
+for(let i=0; i<yourArray.length; i++){
+	 var arr = document.createElement("p");
+        arr.textContent = yourArray[i];
+        Demo.appendChild(arr);
+}
 
